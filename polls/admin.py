d135bbc20b37ce5dbac41ff0844ba2cc6e3b27b4
Sebastiano di Gauso <sebastian.gaus@enders-training.de>
2025-06-02 10:50:27 +0200

    Models Spaß und Queries

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Example, Question, Choice
 
 # Register your models here.
+admin.site.register(Example)
+admin.site.register(Question)
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Example(models.Model):
-    title = models.CharField(max_length=100, primary_key=True)
+    title = models.CharField(max_length=100)
     content = models.TextField()
     date = models.DateField(auto_now=True)
     views_count = models.IntegerField(default=10)
@@ -13,11 +13,20 @@ class Example(models.Model):
         ("D", "Divers")
     ])
     
+    def __str__(self):
+        return self.title
+    
 class Question(models.Model):
     question_text = models.CharField(max_length=200, verbose_name="Fragetext")
     pub_date = models.DateTimeField(auto_now=True, verbose_name="Veröffentlichungsdatum")
+    
+    def __str__(self):
+        return self.question_text
     
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name="Zugehörige Frage")
     choice_text = models.CharField(max_length=200, verbose_name="Antworttext")
     votes = models.IntegerField(default=0, verbose_name="Stimmen")
+    
+    def __str__(self):
+        return self.choice_text
